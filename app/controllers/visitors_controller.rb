5b220a77baeb02f06a3cb61cafd0fe0137081a21
drdeath <drdeathishere@hotmail.com>
2015-11-14 12:51:06 +0000

    new update

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,6 +1,6 @@
 class VisitorsController < ApplicationController
     
     def index
-    @test = Target.find(1).ipaddress
+    #@test = Target.find(1).ipaddress
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,5 @@
 class User < ActiveRecord::Base
-  
-  geocoded_by :ip_address,
-  :latitude => :lat, :longitude => :lon
-after_validation :geocode 
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
