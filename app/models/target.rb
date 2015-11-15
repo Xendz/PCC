99be80dc8c61498198696c8658f3792353ede13c
drdeath <drdeathishere@hotmail.com>
2015-11-15 16:04:08 +0000

    Auto geolocation

--- app/models/target.rb
+++ app/models/target.rb
@@ -1,6 +1,7 @@
 class Target < ActiveRecord::Base
-validates :latitude, :longitude, :presence => true
-geocoded_by :ipaddress
+#validates :latitude, :longitude, :presence => true
+geocoded_by :ipaddress,
+  :latitude => :latitude, :longitude => :longitude
 after_validation :geocode
 
 end
