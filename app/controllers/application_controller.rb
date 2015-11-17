ba12b2680376d42b4378be636603b5b14f4358a1
drdeath <drdeathishere@hotmail.com>
2015-11-17 15:11:04 +0000

    design

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
  
-  
-  protect_from_forgery with: :exception
+
+protect_from_forgery with: :exception
+
+
+
+
 
 def authorize_admin
   if current_user.try(:admin?)
@@ -15,7 +19,6 @@ def authorize_admin
 end
 
   
-
   
 def shodan_method(ipparam)
 
@@ -27,15 +30,3 @@ response = Net::HTTP.get_response(URI.parse("https://api.shodan.io/shodan/host/#
 end
 
 end
-
-
-#Latitude: <%=  @r["latitude"] %> 
-
-# On save form submit method to check LAT and LONG
-  # Store results in DB
-  
-  # Have loop on visitors page go through and display all lat and longs
-
-
-#@target = Target.pluck(:ipaddress)
- # @test = @target.each do | grab |
